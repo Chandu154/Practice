8ef47ad4c6c318f472124eeea85a2311fd7372e2
Chandu154 <devarachandra4@gmail.com>
2023-08-25 17:14:30 +0530

    Practecing The shell

--- practice2.sh
+++ practice2.sh
@@ -1,5 +1,7 @@
 
 yum install git -y 
+yum install nginx
+#code to  run with root access
 
 USERID=$(id -u)
 
@@ -11,6 +13,8 @@ else
   echo "Installing with root access" 
 fi    
 
+#checking the previous command succes or not with exit status
+
 if [ $? -ne 0 ]
 then 
   echo "Installing git is failure"
@@ -19,3 +23,24 @@ else
   echo "Installing git success" 
 fi    
 
+#FUNCTIONS
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+then 
+  echo "Installing $2 is failure"
+  exit 1
+else
+  echo "Installing $2 success" 
+fi    
+}
+
+
+yum install git -y 
+
+VALIDATE $? "git"
+
+yum install mysql
+
+
+VALIDATE $? "mysql"
