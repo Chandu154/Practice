6644fca4049cf1a5dcd957cbcc382db4bf4d13ce
Chandu154 <devarachandra4@gmail.com>
2023-08-25 15:45:08 +0530

    Practecing The shell

--- Practice1.sh
+++ Practice1.sh
@@ -19,3 +19,14 @@ SUM=$((VAR +NAME))
 echo "$SUM"
 
 #for i in 
+
+echo "please enter username"
+
+read -s USERNAME # this is the variable where the value of  one user enterd
+
+echo "user entered is:$USERNAME"
+
+echo "please enter password"
+
+read -s PASSWORD 
+ echo "password entered is:$PASSWORD"
